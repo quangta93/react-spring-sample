b127d1cc677c6f5baf772a98eaf914333fa5a469
Quang Tran <quangta93@gmail.com>
2019-05-12 23:30:21 -0500

    animated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useTransition, animated, config } from 'react-spring';
 import styled from 'styled-components';
+import { lorem, uid } from './helpers';
 
 
 const useMessages = () => {
@@ -8,7 +9,7 @@ const useMessages = () => {
 
   const add = () => {
     const text = lorem();
-    set(list => [...list, { id: id(), text }]);
+    set(list => [...list, { id: uid(), text }]);
   };
 
   const remove = id => set(list => list.filter(item => item.id !== id));
@@ -28,15 +29,13 @@ const Queue = styled.div`
   flex-direction: column;
 `;
 
-const Message = styled(animated.div)`
+const MessageContent = styled.div`
   width: 200px;
-  height: auto;
   padding: 12px;
   font-size: 1rem;
   border-radius: 4px;
   color: white;
   background-color: #445159;
-  margin-top: 15px;
   overflow: hidden;
 `;
 
@@ -49,23 +48,23 @@ const Container = styled.div`
 `;
 
 const MessageQueue = ({ messages, remove }) => {
-  const [refMap] = useState(() => new WeakMap());
+  const [refMap] = useState(new WeakMap());
   // const [cancelMap] = useState(() => new WeakMap());
 
   const transitions = useTransition(
     messages,
     msg => msg.id,
     {
-      from: { height: 0 },
+      from: { opacity: 0, height: 0 },
       enter: msg => async next => await next({
-        height: refMap.get(msg).offsetHeight,
+        opacity: 1,
+        height: refMap.get(msg).offsetHeight + 15,
       }),
-      // leave: msg => async (next, cancel) => {
-      //   // cancelMap.set(msg, cancel);
-      //   await next({ height: 0 });
-      // },
-      leave: { height: 0 },
-      onRest: () => console.log('animation stopped'),
+      leave: msg => async (next, cancel) => {
+        // cancelMap.set(msg, cancel);
+        await next({ opacity: 0, height: 0 });
+      },
+      // onRest: () => console.log('animation stopped'),
     },
   );
 
@@ -73,9 +72,9 @@ const MessageQueue = ({ messages, remove }) => {
     <Queue>
       {transitions.map(
         ({ item, key, props }) => (
-          <div
+          <animated.div
             key={key}
-            style={{ height: props.height }}
+            style={props}
             onClick={e => {
               e.preventDefault();
               e.stopPropagation();
@@ -83,28 +82,17 @@ const MessageQueue = ({ messages, remove }) => {
               remove(item.id);
             }}
           >
-            <Message ref={ref => refMap.set(item, ref)}>
+            <MessageContent ref={ref => refMap.set(item, ref)}>
               {item.id}
               <br/>
               {item.text}
-            </Message>
-          </div>
+            </MessageContent>
+          </animated.div>
         )
       )}
     </Queue>
   );
 };
-
-const Sentences = [
-  'jQuery is a chain of creating user interfaces based on the increasing speed of objects to help run both in or included from HTML pages.',
-  'Currying is a child function.',
-  'Closure is an ecosystem for a function with first-class functions, making the majority of page refresh.',
-  'Passport.',
-  'Arity is a package manager with a function calls by a JavaScript code can be easily referenced.',
-]
-
-const lorem = () => Sentences[Math.floor(Math.random() * 5)];
-const id = () => `_${Math.random().toString(36).slice(2, 9)}`;
 
 export default () => {
   const { messages, add, remove } = useMessages();
---
+++ src/helpers.js
@@ -0,0 +1,11 @@
+
+const Sentences = [
+  'jQuery is a chain of creating user interfaces based on the increasing speed of objects to help run both in or included from HTML pages.',
+  'Currying is a child function.',
+  'Closure is an ecosystem for a function with first-class functions, making the majority of page refresh.',
+  'Passport.',
+  'Arity is a package manager with a function calls by a JavaScript code can be easily referenced.',
+];
+
+export const lorem = () => Sentences[Math.floor(Math.random() * 5)];
+export const uid = () => `_${Math.random().toString(36).slice(2, 9)}`;
