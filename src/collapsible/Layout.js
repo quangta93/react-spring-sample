420632e413f3e7e4a2efa3240e662068db406341
Quang Tran <quangta93@gmail.com>
2019-06-11 13:33:30 -0500

    placeholder for new sample

---
+++ src/collapsible/Layout.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+
+export const Layout = () => <div>Collapsible Layout</div>;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { AlertQueue } from './AlertQueue';
+import { Layout } from './collapsible';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AlertQueue />, document.getElementById('root'));
+ReactDOM.render(<Layout />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
